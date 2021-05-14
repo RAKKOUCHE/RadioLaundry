4cf8af9998fb38deb36aa717a51eb32e0882c5a0
Rachid AKKOUCHE <rachid.akkouche@wanadoo.fr>
2021-05-14 19:37:27 +0200

    Programme de gestion du buffer random créer
    Signed-off-by: Rachid AKKOUCHE <rachid.akkouche@wanadoo.fr>

---
+++ SW/Main/C#/random/random/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace random
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var rand = new Random();
+            var fileName = "random.dat";
+            var buffer = new byte[1024];
+            BinaryWriter bw = new BinaryWriter(File.Open(fileName, FileMode.Create));
+            //for (int i = 0; i < 1024; i++)
+            //{              
+            //    bw.Write(rand.Next());
+            //}
+            rand.NextBytes(buffer);
+            bw.Write(buffer);
+            bw.Close();
+        }
+    }
+}
